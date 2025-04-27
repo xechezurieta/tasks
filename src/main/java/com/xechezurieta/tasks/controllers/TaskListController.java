c9f27f10ea2753fd65e2955452ca447aa02515af
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-04-27 21:59:41 +0200

    feat: add TaskListController for managing task lists

---
+++ src/main/java/com/xechezurieta/tasks/controllers/TaskListController.java
@@ -0,0 +1,31 @@
+package com.xechezurieta.tasks.controllers;
+
+import com.xechezurieta.tasks.domain.dto.TaskListDto;
+import com.xechezurieta.tasks.mappers.TaskListMapper;
+import com.xechezurieta.tasks.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
